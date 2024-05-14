e51fa2ab30d16f2a4d76df3d5f0c2a284d597fff
sanket0802 <93255738+sanket0802@users.noreply.github.com>
2024-05-14 16:23:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'otto.db.elephantsql.com',
-    port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'cklijfef',
-    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-    database: env.DB_NAME || 'cklijfef',
+    host: env.POSTGRES_HOST || 'ep-small-river-a4pf78lk-pooler.us-east-1.aws.neon.tech',
+  //  port: env.DB_PORT || '5432',
+    user: env.POSTGRES_USER || 'default',
+    password: env.POSTGRES_PASSWORDD || 'PEIXl7nrGfm4',
+    database: env.POSTGRES_DATABASE || 'verceldb',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
